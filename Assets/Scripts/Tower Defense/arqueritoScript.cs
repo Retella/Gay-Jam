2a9e5d0ce1339965f25c2152dad188d95fb0e1a6
Retella <agtobarruela@gmail.com>
2026-07-24 18:27:17 +0200

    Velocidad de seguimiento aleatoria

--- Assets/Scripts/Tower Defense/arqueritoScript.cs
+++ Assets/Scripts/Tower Defense/arqueritoScript.cs
@@ -4,7 +4,14 @@ public class arqueritoScript : MonoBehaviour
 {
     private bool isFollowing = false;
     private Transform targetToFollow;
+    private float followSpeed;
 
+
+    public void Start()
+    {
+        // Inicializa la velocidad de seguimiento aleatoria
+        followSpeed = 2f * Random.Range(0.5f, 3f);
+    }
     public void follow(Transform target)
     {
         isFollowing = true;
@@ -15,7 +22,7 @@ public class arqueritoScript : MonoBehaviour
     {
         if (isFollowing && targetToFollow != null)
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetToFollow.position, (targetToFollow.position - transform.position).magnitude * 2f * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, targetToFollow.position, (targetToFollow.position - transform.position).magnitude * followSpeed * Time.deltaTime);
         }
     }
 }
