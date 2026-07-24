6f187789bf1750bd1f02c8dd941e8fa7389c4030
Retella <agtobarruela@gmail.com>
2026-07-24 17:49:43 +0200

    carrerita hecha con flechitas rojas

--- Assets/Scripts/chavaloScript.cs
+++ Assets/Scripts/chavaloScript.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,7 @@ public class chavaloScript : MonoBehaviour
 {
 [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float smoothTime = 0.1f;
+    [SerializeField] private CinemachineFollow cinemachineFollow;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
@@ -12,6 +14,9 @@ public class chavaloScript : MonoBehaviour
     private Vector2 velocitySmoothing;
 
     private InputSystem_Actions inputActions; // clase generada
+
+    private bool isCarrerita = false;
+    private Vector2 carreritaDirection;
 
     private void Awake()
     {
@@ -40,9 +45,45 @@ public class chavaloScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 targetVelocity = moveInput * moveSpeed;
-        currentVelocity = Vector2.SmoothDamp(currentVelocity, targetVelocity, ref velocitySmoothing, smoothTime);
-        rb.linearVelocity = currentVelocity; // en Unity 6 es "linearVelocity"; en versiones anteriores usa "velocity"
+        if (isCarrerita)
+        {
+            rb.linearVelocity = carreritaDirection * moveSpeed * 2.5f;
+        }
+        else
+        {
+            Vector2 targetVelocity = moveInput * moveSpeed;
+            currentVelocity = Vector2.SmoothDamp(currentVelocity, targetVelocity, ref velocitySmoothing, smoothTime);
+            rb.linearVelocity = currentVelocity; // en Unity 6 es "linearVelocity"; en versiones anteriores usa "velocity"
+        }
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("carreritaTag"))
+        {
+            if (isCarrerita)
+            {
+                desactivarCarrerita();
+            }
+            else
+            {
+                activarCarrerita(collision.gameObject.transform);
+            }
+        }
+    }
+
+    private void activarCarrerita(Transform carreritaTransform)
+    {
+        isCarrerita = true;
+        carreritaDirection = carreritaTransform.up;
+        cinemachineFollow.FollowOffset.x = carreritaTransform.up.x * 15f; // Ajusta la cámara para seguir la carrerita
+        cinemachineFollow.FollowOffset.y = carreritaTransform.up.y * 15f; // Ajusta la cámara para seguir la carrerita
+    }
+
+    private void desactivarCarrerita()
+    {
+        isCarrerita = false;
+        carreritaDirection = Vector2.zero;
+        cinemachineFollow.FollowOffset = new Vector3(0f, 0f, -10f); // Ajusta la cámara para dejar de seguir la carrerita
     }
 }
