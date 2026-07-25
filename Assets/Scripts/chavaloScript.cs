d4fd7e2a116c1608f1ca39c93e3c0c6a36b855ad
Retella <agtobarruela@gmail.com>
2026-07-25 16:48:05 +0200

    ara te puedes mover durante el runner

--- Assets/Scripts/chavaloScript.cs
+++ Assets/Scripts/chavaloScript.cs
@@ -49,6 +49,9 @@ public class chavaloScript : MonoBehaviour
         if (isCarrerita)
         {
             rb.linearVelocity = carreritaDirection * moveSpeed * 2.5f;
+            Vector2 targetVelocity = moveInput * moveSpeed;
+            currentVelocity = Vector2.SmoothDamp(currentVelocity, targetVelocity, ref velocitySmoothing, smoothTime);
+            rb.linearVelocity += currentVelocity;
         }
         else
         {
